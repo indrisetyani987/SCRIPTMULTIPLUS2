994a3c1616920e2b739643ebbcfd50dfe09bc3dd
indrisetyani987 <indrisetyani987@gmail.com>
2025-12-16 22:49:22 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -830,6 +830,80 @@ print_success "All Packet"
 #Instal Menu
 function menu(){
 clear
+mkdir -p /etc/rmbl/theme
+cat <<EOF>> /etc/rmbl/theme/green
+BG : \E[40;1;42m
+TEXT : \033[0;32m
+EOF
+cat <<EOF>> /etc/rmbl/theme/yellow
+BG : \E[40;1;43m
+TEXT : \033[0;33m
+EOF
+cat <<EOF>> /etc/rmbl/theme/red
+BG : \E[40;1;41m
+TEXT : \033[0;31m
+EOF
+cat <<EOF>> /etc/rmbl/theme/blue
+BG : \E[40;1;44m
+TEXT : \033[0;34m
+EOF
+cat <<EOF>> /etc/rmbl/theme/magenta
+BG : \E[40;1;45m
+TEXT : \033[0;35m
+EOF
+cat <<EOF>> /etc/rmbl/theme/cyan
+BG : \E[40;1;46m
+TEXT : \033[0;36m
+EOF
+cat <<EOF>> /etc/rmbl/theme/lightgray
+BG : \E[40;1;47m
+TEXT : \033[0;37m
+EOF
+cat <<EOF>> /etc/rmbl/theme/darkgray
+BG : \E[40;1;100m
+TEXT : \033[0;90m
+EOF
+cat <<EOF>> /etc/rmbl/theme/lightred
+BG : \E[40;1;101m
+TEXT : \033[0;91m
+EOF
+cat <<EOF>> /etc/rmbl/theme/lightgreen
+BG : \E[40;1;102m
+TEXT : \033[0;92m
+EOF
+cat <<EOF>> /etc/rmbl/theme/lightyellow
+BG : \E[40;1;103m
+TEXT : \033[0;93m
+EOF
+cat <<EOF>> /etc/rmbl/theme/lightblue
+BG : \E[40;1;104m
+TEXT : \033[0;94m
+EOF
+cat <<EOF>> /etc/rmbl/theme/lightmagenta
+BG : \E[40;1;105m
+TEXT : \033[0;95m
+EOF
+cat <<EOF>> /etc/rmbl/theme/lightcyan
+BG : \E[40;1;106m
+TEXT : \033[0;96m
+EOF
+cat <<EOF>> /etc/rmbl/theme/color.conf
+lightcyan
+EOF
+
+mkdir -p /etc/rmbl/warnafont
+cat <<EOF>> /etc/rmbl/warnafont/fontgren
+WARNAF : \033[1;92m
+EOF
+cat <<EOF>> /etc/rmbl/warnafont/fontcyan
+WARNAF : \033[1;96m
+EOF
+cat <<EOF>> /etc/rmbl/warnafont/fontlight
+WARNAF : \033[1;37m
+EOF
+cat <<EOF>> /etc/rmbl/warnafont/warnaf.conf
+fontlight
+EOF
 cd
 print_install "Memasang Menu Packet"
 wget https://raw.githubusercontent.com/indrisetyani987/SCRIPTMULTIPLUS2/main/menu.zip
